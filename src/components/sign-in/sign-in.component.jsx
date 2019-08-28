079dbc02cd1a0bfdb23affef39c283aa468d4f1c
XiangLingChen <belle19950901@gmail.com>
2019-08-28 16:13:29 +0800

    sign-in-link-to-sign-up-page-RWD

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import {Link} from 'react-router-dom';
 import {signInWithGoogle} from '../../components/firebase/firebase.utils';
 import './sign-in.style.css';
 
@@ -47,6 +48,11 @@ class SignIn extends React.Component {
                      handleChange={this.handleChange}
                      required
                      />
+
+                     <div className='goToSignUp'>
+                        <p className='no-account'>Don't have an account?</p>
+                        <Link className='link-to-sign-up' to='/signup'>Register Now.</Link>
+                     </div>
                     
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
