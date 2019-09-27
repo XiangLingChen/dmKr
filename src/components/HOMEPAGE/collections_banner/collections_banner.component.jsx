50115eb192499603a44dadc8a756b55c08bdc535
XiangLingChen <belle19950901@gmail.com>
2019-09-28 00:43:19 +0800

    directory page RWD

--- src/components/HOMEPAGE/collections_banner/collections_banner.component.jsx
+++ src/components/HOMEPAGE/collections_banner/collections_banner.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import {withRouter} from 'react-router-dom';
 import './collections_banner.style.css'
 
-const CollectionsBanner = () => (
+const CollectionsBanner = ({history}) => (
     <div className='CollectionsBanner_container'>
         <div className='CollectionsBanner_content'>
             <p className='CollectionsBanner_text'> WE INNOVATES EFFECTIVE FORMULAS TO</p>
             <p className='CollectionsBanner_text'> DELIVER RESULTS FOR YOUR CONCERNS. </p>
             <div className='CTA_container'>
-                <span className='CollectionsBanner_CTA'>Shop by Collections</span>
+                <span className='CollectionsBanner_CTA' onClick={ ()=> history.push('./directory')}>Shop by Collections</span>
             </div>
         </div>
     </div>
 );
 
-export default CollectionsBanner;
+export default withRouter(CollectionsBanner);
