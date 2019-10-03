321c774f5dd989bf0c5e4d800b5318727ee29520
XiangLingChen <belle19950901@gmail.com>
2019-10-03 19:27:56 +0800

    adding addItem cart.action from redux at shop-all

--- src/components/Shop-all/Shop-all-card.component.jsx
+++ src/components/Shop-all/Shop-all-card.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.action';
 import './Shop-all-card.style.css';
 
 
 
-const ShopAllCardDetail = ({item}) => {
+const ShopAllCardDetail = ({item, addItem}) => {
 
     const {name, price, imageUrl} = item;
  
@@ -20,13 +21,16 @@ const ShopAllCardDetail = ({item}) => {
                 </div>
             </div>
     
-            <button className="shop-all-custom-btn">
+            <button className="shop-all-custom-btn" onClick = { () => addItem(item) }>
                 Add to Cart
             </button>
         </div>
     );
 }
 
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
 
 
-export default ShopAllCardDetail;
+export default connect(null, mapDispatchToProps)(ShopAllCardDetail);
--- src/components/Shop-all/Shop-all-product.component.jsx
+++ src/components/Shop-all/Shop-all-product.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ShopAllProductList from './Shop-all-product-list';
+
 import ShopAllCardDetail from './Shop-all-card.component';
 
 
@@ -10,16 +10,109 @@ class ShopAllProducts extends React.Component {
         super();
 
         this.state = {
-            allProductOverview: ShopAllProductList.items
+            items : [
+                {
+                    id: 0,
+                    name: 'Beauty Effect Ampoule',
+                    imageUrl: 'https://imgur.com/5RIBTFf.jpg',
+                    price: 20
+                },
+                {
+                    id: 1,
+                    name: 'Beauty Effect Hyaluron Booster',
+                    imageUrl: 'https://imgur.com/umzIG1X.jpg',
+                    price: 16
+                },
+                {
+                    id: 2,
+                    name: 'Beauty Effect Day Cream',
+                    imageUrl: 'https://imgur.com/EotsUGC.jpg',
+                    price: 18
+                },
+                {
+                    id: 3,
+                    name: 'Beauty Effect Night Cream',
+                    imageUrl: 'https://imgur.com/0DnPZlt.jpg',
+                    price: 18
+                },
+                {
+                    id: 4,
+                    name: 'Aqua Serum',
+                    imageUrl: 'https://imgur.com/NRsMCHg.jpg',
+                    price: 18
+                },
+                {
+                    id: 5,
+                    name: 'Aqua Eye Roll-On',
+                    imageUrl: 'https://imgur.com/ZAskFYQ.jpg',
+                    price: 15
+                },
+                {
+                    id: 6,
+                    name: 'Aqua Day Cream',
+                    imageUrl: 'https://imgur.com/Z4Lfb6R.jpg',
+                    price: 18
+                },
+                {
+                    id: 7,
+                    name: 'Q10 Serum',
+                    imageUrl: 'https://imgur.com/3NFMq0m.jpg',
+                    price: 18
+                },
+                {
+                    id: 8,
+                    name: 'Q10 Eye Cream',
+                    imageUrl: 'https://imgur.com/IJ4FyCH.jpg',
+                    price: 16
+                },
+                {
+                    id: 9,
+                    name: 'Q10 Concentrate',
+                    imageUrl: 'https://imgur.com/LMbKvgt.jpg',
+                    price: 22
+                },
+                {
+                    id: 10,
+                    name: 'Q10 Day Cream',
+                    imageUrl: 'https://imgur.com/Dubsmwh.jpg',
+                    price: 20
+                },
+                {
+                    id: 11,
+                    name: 'Q10 Night Cream',
+                    imageUrl: 'https://imgur.com/h1OWW36.jpg',
+                    price: 20
+                },
+                {
+                    id: 12,
+                    name: 'Aloe Vera Eye Cream',
+                    imageUrl: 'https://imgur.com/O6KFJo4.jpg',
+                    price: 15
+                },
+                {
+                    id: 13,
+                    name: 'Lip',
+                    imageUrl: 'https://imgur.com/EA0VORF.jpg',
+                    price: 9
+                },
+                {
+                    id: 14,
+                    name: 'Lip',
+                    imageUrl: 'https://imgur.com/iI1AYLc.jpg',
+                    price: 9
+                }
+        
+        
+            ]
         }
     }
 
     render() {
-        const {allProductOverview} = this.state;
+     
         return(
                     <div className="shop-all-page-overview">
                         {
-                            allProductOverview.map( (item) => (
+                            this.state.items.map( (item) => (
                                 <ShopAllCardDetail key={item.id} item = {item}/>
                             ))
                         }
