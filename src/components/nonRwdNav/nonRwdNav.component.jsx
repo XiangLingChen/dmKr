7e83a351381511f333b20bf17fbe3cbd6c13c40c
XiangLingChen <belle19950901@gmail.com>
2019-10-17 17:02:16 +0800

    navBar hover dropdown RWD

--- src/components/nonRwdNav/nonRwdNav.component.jsx
+++ src/components/nonRwdNav/nonRwdNav.component.jsx
@@ -125,8 +125,76 @@ const Toolbar = ({currentUser, hidden, directoryDropdown, history}) => (
         <nav className="mobile_navigation">
             <div className='mobile-options-container'>
                 <div className='mobile-options1'>
-                    <Link className='option' to='/shopall'>SHOP ALL</Link>
-                    <Link className='option' to='/bestSellers'>BEST-SELLERS</Link>
+                    <div className='shopall-option'>
+                        <Link className='option shopAll' to='/shopall'>SHOP ALL</Link>
+                            <div className='shopall-dropdown'>
+                                <div className='shopall-dropdown-content'>
+                                    <div className='shopall-dropdown-col1'>
+                                        <div className='dropdown-directory'>SKIN CARE</div>
+                                            <div className='dropdown-list-of-series'>
+                                                <Link className='dropdown-series' to='/shopall/beautyEffect'>Beauty Effect</Link>
+                                                <Link className='dropdown-series' to='/shopall/aqua'>Aqua</Link>
+                                                <Link className='dropdown-series' to='/shopall/q10'>Q10</Link>
+                                                <Link className='dropdown-series' to='/shopall/aloeVera'>Aloe Vera</Link>
+                                                <Link className='dropdown-series' to='/shopall/almond'>Almond</Link>
+                                                <Link className='dropdown-series' to='/shopall/lipBalm'>Lip</Link>
+                                            </div>
+                                        </div>
+                                    <div className='shopall-dropdown-col2'>
+                                        <div className='dropdown-directory'>CLEANSING</div>
+                                            <div className='dropdown-list-of-series'>
+                                                <Link className='dropdown-series' to='/shopall/makeupRemover'>Makeup Remover</Link>
+                                                <Link className='dropdown-series' to='/shopall/cleansing'>Cleansing</Link>
+                                            </div>
+                                        <div className='dropdown-directory'>BODY CARE</div>
+                                            <div className='dropdown-list-of-series'>
+                                                <Link className='dropdown-series' to='/shopall/bodyMilk'>Body Milk</Link>
+                                                <Link className='dropdown-series' to='/shopall/bodyFit'>Body Fit</Link>
+                                            </div>
+                                        <div className='dropdown-directory'>HAIR</div>
+                                            <div className='list-of-series'>
+                                                <Link className='dropdown-series' to='/shopall/shampoo'>Shampoo</Link>
+                                                <Link className='dropdown-series' to='/shopall/conditioner'>Conditioner</Link>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+                    <div className='bestseller-option'>
+                        <Link className='option bestseller' to='/bestSellers'>BEST-SELLERS</Link>
+                            <div className='bestSellers-dropdown'>
+                                <div className='bestSellers-dropdown-content'>
+                                    <Link className='bestSellers-dropdown-frame'   to='/bestSellers/skincare'>
+                                        <img className='bestSellers-dropdown-img' src={SKINCARE} alt='' />
+                                        <div>
+                                            <p className='bestSellers-dropdown-title'>SKIN CARE</p>
+                                        </div>
+                                    </Link>
+                                
+                                    <Link className='bestSellers-dropdown-frame'  to='/bestSellers/cleansing'>
+                                        <img className='bestSellers-dropdown-img' src={CLEANSING} alt='' />
+                                        <div>
+                                            <p className='bestSellers-dropdown-title'>CLEANSING</p>
+                                        </div>
+                                    </Link >
+                                
+                                    <Link className='bestSellers-dropdown-frame'  to='/bestSellers/bodycare'>
+                                        <img className='bestSellers-dropdown-img' src={BODYCARE} alt='' />
+                                        <div>
+                                            <p className='bestSellers-dropdown-title'>BODY CARE</p>
+                                        </div>
+                                    </Link >
+                            
+                                    <Link className='bestSellers-dropdown-frame'  to='/bestSellers/hair'>
+                                        <img className='bestSellers-dropdown-img' src={HAIR} alt='' />
+                                        <div>
+                                            <p className='bestSellers-dropdown-title'>HAIR</p>
+                                        </div>
+                                    </Link >
+                                </div>
+                            </div>
+                    </div>
                     <Link className='option' to='/about'>ABOUT</Link>
                 </div>
             </div>
